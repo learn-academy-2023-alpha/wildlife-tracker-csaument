68e82025c3186f1e64de0dd00ce1911419464aff
csaument <c.s.aument@gmail.com>
2023-03-31 12:02:24 -0700

    adds validation for animal and animal sightings models. passes 404 and 422 errors with invalid operations

--- spec/models/animal_sighting_spec.rb
+++ spec/models/animal_sighting_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe AnimalSighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid data' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      sighting = animal.animal_sightings.create(latitude: 30.0, longitude: -120.0, date: "2023-03-30")
+      expect(sighting.errors).to be_empty
+  end
+
+  it 'is not valid without a lat' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      sighting = animal.animal_sightings.create(longitude: -120.0, date: "2023-03-30")
+      expect(sighting.errors[:latitude]).to_not be_empty
+  end
+
+  it 'is not valid if latitude is out of -90..90' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      sighting = animal.animal_sightings.create(latitude: 100.0, longitude: -120.0, date: "2023-03-30")
+      expect(sighting.errors[:latitude]).to_not be_empty
+  end
+
+  it 'is not valid without a long' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      sighting = animal.animal_sightings.create(latitude: 30.0, date: "2023-03-30")
+      expect(sighting.errors[:longitude]).to_not be_empty
+  end
+
+  it 'is not valid if longitude is out of -180..180' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      sighting = animal.animal_sightings.create(latitude: 30.0, longitude: -200.0, date: "2023-03-30")
+      expect(sighting.errors[:longitude]).to_not be_empty
+  end
+
+  it 'is not valid without a date' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      sighting = animal.animal_sightings.create(latitude: 30.0, longitude: -120.0)
+      expect(sighting.errors[:date]).to_not be_empty
+  end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid data' do
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      expect(animal.errors).to be_empty
+  end
+
+  it 'is not valid without a common name' do
+      animal = Animal.create scientific_binomial: 'Canis familiaris'
+      expect(animal.errors[:common_name]).to_not be_empty
+  end
+
+  it 'is not valid without a scientific binomial' do
+      animal = Animal.create common_name: 'Dog'
+      expect(animal.errors[:scientific_binomial]).to_not be_empty
+  end
+
+  it 'is not valid if common name is scientific binomial' do
+      animal = Animal.create common_name: 'Canis familiaris', scientific_binomial: 'Canis familiaris'
+      expect(animal.errors[:common_name]).to_not be_empty
+  end
+
+  it 'is not valid if common name is not unique' do
+      Animal.create common_name: 'Dog', scientific_binomial: 'Feline familiaris'
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      expect(animal.errors[:common_name]).to_not be_empty
+  end
+
+  it 'is not valid if scientfic binomial is not unique' do
+      Animal.create common_name: 'Cat', scientific_binomial: 'Canis familiaris'
+      animal = Animal.create common_name: 'Dog', scientific_binomial: 'Canis familiaris'
+      expect(animal.errors[:scientific_binomial]).to_not be_empty
+  end
 end
